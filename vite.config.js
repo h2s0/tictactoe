cd2d3d17d2a6364dfa12744ee1c634dd8633c23d
heeso <khs@alpinelab.io>
2024-05-22 15:47:56 +0900

    ✨ rollup-plugin-obfuscator 설치

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,22 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import { env } from "node:process";
+import obfuscator from 'rollup-plugin-obfuscator';
 
 const idDev = env.NODE_ENV === "development";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
+ plugins: [
+  react(),
+  obfuscator({
+    controlFlowFlattening: true,
+    rotateStringArray: true,
+    deadCodeInjection: true,
+    disableConsoleOutput: true,
+    stringArray: true,
+  })
+],
  css: {
    devSourcemap: true,
    modules: {
