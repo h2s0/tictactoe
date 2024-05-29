ee6ba2f6a0826783ab046be0949f40b6c2f62893
heeso <khs@alpinelab.io>
2024-05-29 16:24:20 +0900

    ✨ flag 값 수정 및 zustand 사용

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,19 @@
+import { useStore } from '@/store/store';
+
 function Title ({ p, c }) {
+  const items = useStore(state => state.items);
+
+  const z = String.fromCharCode(items[6] + items[0]);
+  const y = String.fromCharCode(105);
+  const x =String.fromCharCode(87);
+
+  const text1 = `${x}i$d${items[5]}m beg${y}ns i${z} w${items[5]}nder`;
+  const text2 = `Th2s to0${items[8]} sh@l1 p${items[1]}s$`;
+
   return(
     <>
-      {p === 9999 && <h5>w1nner w1nner ch1cken dinner</h5>}
-      {c === 9999 && <h5>loser loser drunken b00zer</h5>}
+      {p === 9999 && <h5>{text1}.</h5>}
+      {c === 9999 && <h5>!{text2}</h5>}
     </>
   )
 }
